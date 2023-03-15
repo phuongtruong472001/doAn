be58a823baa621f017ae6d957d7d1cce602d00c3
phuongtruong472001 <71766470+phuongtruong472001@users.noreply.github.com>
2023-03-15 23:25:08 +0700

    create project

---
+++ do_an/lib/base/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color blue = Color.fromRGBO(0, 113, 255, 1);
+  static const Color black = Color.fromRGBO(18, 24, 38, 1);
+  static const Color white = Color.fromRGBO(255, 255, 255, 1);
+  static const Color grey = Color.fromRGBO(146, 146, 146, 1);
+}
