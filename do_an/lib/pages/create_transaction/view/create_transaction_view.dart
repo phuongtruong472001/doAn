7cd5250ebe89340d8e2422bc101735146469ec4a
phuongtruong472001 <71766470+phuongtruong472001@users.noreply.github.com>
2023-03-26 20:48:44 +0700

    create folder

---
+++ do_an/lib/pages/create_transaction/view/create_transaction_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controller/create_transaction_controller.dart';
+
+class CreateTransactionPage extends GetView<CreateTransactionController> {
+    const CreateTransactionPage({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(),
+    );
+    }
+}
---
+++ do_an/lib/pages/detail_transaction/view/detail_transaction_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controller/detail_transaction_controller.dart';
+
+class DetailTransactionPage extends GetView<DetailTransactionController> {
+    const DetailTransactionPage({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(),
+    );
+    }
+}
